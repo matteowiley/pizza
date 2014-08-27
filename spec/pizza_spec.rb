11f77f0d4e72191e7b6e431cb0f900080c3ade01
Matteo Wiley <matteowiley@gmail.com>
2014-08-27 17:07:29 -0500

    This test failed because it doesn't default to just cheese.

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -9,15 +9,18 @@ describe Pizza do
       ]
       pizza = Pizza.new(toppings)
     
-    expect(pizza.toppings).to be_a(toppings)
+    expect(pizza.toppings).to be_a(Array)
+    end
+    it 'defaults the toppings to cheese only, if the pizza has no toppings' do
+      pizza = Pizza.new
+
+      expect(pizza.toppings.size).to eq(1)
+      expect(pizza.toppings.first.name).to eq('cheese')
     end
   end
 end
 
 describe Topping do
-  it "exists" do
-    expect(Topping).to be_a(Class)
-  end
   describe '.initialize' do 
     it "sets the name of the topping" do 
       topping = Topping.new 'bell pepper', vegetarian: true
